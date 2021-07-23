5dc6ffe1b3ef45903c4b433f6f0e10803a967bdc
sp <onyangoricks@gmail.com>
2021-07-23 10:55:24 +0300

    add default date and form validations

--- app/src/components/task/create.vue
+++ app/src/components/task/create.vue
@@ -2,12 +2,14 @@
   <q-form @submit="add_task">
     <q-card-section>
       <label>Task title</label>
-      <q-input placeholder="Going shopping" outlined square dense v-model="task.title" color="grey-5"/>
+      <q-input  lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Enter title to continue']" placeholder="Going shopping" outlined square dense v-model="task.title" color="grey-5"/>
     </q-card-section>
 
     <q-card-section class="q-pt-none">
       <label>Due date</label>
-      <q-input dense square outlined color="grey-5" v-model="task.due_date">
+      <q-input dense square outlined color="grey-5"  lazy-rules
+               :rules="[ val => val && val.length > 0 && !date_in_the_past || 'Enter valid date and time to continue']" v-model="task.due_date">
         <template v-slot:prepend>
           <q-icon name="event" class="cursor-pointer">
             <q-popup-proxy transition-show="scale" transition-hide="scale">
@@ -36,7 +38,8 @@
 
     <q-card-section>
       <label>Status</label>
-      <q-select
+      <q-select  lazy-rules
+                 :rules="[ val => val && val.length > 0 || 'Select Status']"
         dense square outlined
         color="grey-5"
         v-model="task.status"
@@ -45,12 +48,13 @@
       />
     </q-card-section>
     <q-card-section>
-      <q-btn class="full-width" unelevated color="green" label="Add Task" no-caps/>
+      <q-btn type="submit" class="full-width" unelevated color="green" label="Add Task" no-caps/>
     </q-card-section>
   </q-form>
 </template>
 
 <script>
+import { date } from 'quasar'
 export default {
   name: "create",
   data(){
@@ -66,9 +70,23 @@ export default {
   computed: {
     statuses(){
       return ["todo", "progress", "done"]
+    },
+    default_due_date(){
+      const { addToDate } = date
+      return date.formatDate( addToDate(new Date(), { hours: 1 }), 'YYYY-MM-DD HH:mm')
+    },
+    date_in_the_past(){
+      let now = new Date();
+      return this.due_date >= now;
     }
   },
+  created(){
+    this.set_default_due_date(this.default_due_date)
+  },
   methods: {
+    set_default_due_date (due_date){
+      return this.task.due_date = due_date
+    },
     add_task(){
       this.submitting = true
       this.$store.dispatch('todo/add_task', this.task).then((r) => {
@@ -78,7 +96,8 @@ export default {
         this.submitting = false
       })
     }
-  }
+  },
+
 }
 </script>
 
