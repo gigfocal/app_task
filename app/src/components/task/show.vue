26a66126d7bf1e8c2945a78d739a0a261bf4e393
sp <onyangoricks@gmail.com>
2021-07-23 08:59:10 +0300

    add app layout and task component

---
+++ app/src/components/task/show.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-card flat class="bg-blue-1">
+   <q-list>
+     <q-item>
+       <q-item-section avatar>
+         <q-radio v-model="status" :val="task.status"/>
+       </q-item-section>
+       <q-item-section>
+         {{task.title}}
+       </q-item-section>
+       <q-item-section avatar>
+         <q-btn flat round icon="more_vert"></q-btn>
+       </q-item-section>
+     </q-item>
+   </q-list>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "show",
+  props: {
+    task: Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- app/src/pages/Index.vue
+++ app/src/pages/Index.vue
@@ -1,17 +1,46 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <div class="row q-col-gutter-lg">
+      <div class="col-md-4 col-xs-12">
+        <div class="text-h6">Todo</div>
+        <div class="q-mt-md" v-for="(todo, key) in todos" :key="key">
+          <show :task="todo"/>
+        </div>
+      </div>
+      <div class="col-md-4 col-xs-12">
+        <div class="text-h6">In Progress</div>
+        <div class="q-mt-md" v-for="(todo, key) in todos" :key="key">
+          <show :task="todo"/>
+        </div>
+      </div>
+      <div class="col-md-4 col-xs-12">
+        <div class="text-h6">Complete</div>
+        <div class="q-mt-md" v-for="(todo, key) in todos" :key="key">
+          <show :task="todo"/>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
+import Show from "components/task/show";
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: {Show},
+  data() {
+    return {
+      todos: [
+        {
+          title: "Go shopping",
+          date: new Date()
+        }
+      ],
+      progress: [],
+      completed: []
+    }
+  }
 })
 </script>
